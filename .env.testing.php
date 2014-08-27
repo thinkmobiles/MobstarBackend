3c68f267152fc32bd61997ffb0b6e080740a5f29
Matthew <matthew@goreport.net>
2014-08-27 23:18:17 +0100

    debug beta

--- .env.testing.php
+++ .env.testing.php
@@ -3,5 +3,5 @@ return array(
     'DATABASE_PASSWORD'	=> 'd0wnund3r',
     'PATH'				=> '/var/www/api-beta/',
 	'URL'				=> 'ec2-54-76-141-75.eu-west-1.compute.amazonaws.com',
-	'DEBUG'				=> false,
+	'DEBUG'				=> true,
 );
