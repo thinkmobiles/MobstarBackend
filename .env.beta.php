a31eeb9a62dbbdb39fc0b2f0596bd682723c64ae
Matthew <matthew@goreport.net>
2014-08-27 22:31:26 +0100

    Environment setup

---
+++ .env.beta.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+    'DATABASE_PASSWORD'	=> 'd0wnund3r',
+    'PATH'				=> '/var/www/api-beta/',
+	'URL'				=> 'ec2-54-76-141-75.eu-west-1.compute.amazonaws.com',
+	'DEBUG'				=> false,
+);
