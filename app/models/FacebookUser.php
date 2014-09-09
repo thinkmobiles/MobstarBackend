9ec4ce776924857d3f927f380762b6cb386d1828
Matthew <matthew@goreport.net>
2014-09-09 22:32:15 +0100

    Facebook user stuff

---
+++ app/models/FacebookUser.php
@@ -0,0 +1,13 @@
+<?php
+
+class FacebookUser extends \Eloquent {
+	protected $fillable = ['facebook_user_facebook_id', 'facebook_user_display_name', 'facebook_user_user_name'];
+	protected $table = "facebook_users";
+	protected $primaryKey = "facebook_user_id";
+	//protected $guarded = array('twitter_user_id');
+	public $timestamps = false;
+
+	public function User(){
+		return $this->belongsTo('User', 'user_facebook_id');
+	}
+}
