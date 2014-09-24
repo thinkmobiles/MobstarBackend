b508a476de6bd3ffea22770ad56a40f141d2669b
Matthew <matthew@goreport.net>
2014-09-24 08:48:30 +0100

    include total comments in oneEntry method

--- app/helper.php
+++ app/helper.php
@@ -147,6 +147,7 @@ function oneEntry( $entry, $session, $includeUser = false )
 
 	$current[ 'name' ] = $entry->entry_name;
 	$current[ 'description' ] = $entry->entry_description;
+	$current[ 'totalComments' ] = $entry->comments->count();
 	$current[ 'created' ] = $entry->entry_created_date;
 	$current[ 'modified' ] = $entry->entry_modified_date;
 
