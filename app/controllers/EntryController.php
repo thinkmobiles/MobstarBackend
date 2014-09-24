1351fb8303871d61cc1917448727adf551293067
Matthew <matthew@goreport.net>
2014-09-25 00:02:12 +0100

    wip

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -1910,7 +1910,7 @@ class EntryController extends BaseController
 	public function updateFile()
 	{
 
-		$entries = $this->entry->whereIn( 0, 0, 0, 200, 0, false );
+		$entries = $this->entry->all( 0, 0, 0, 200, 0, false );
 		$i = 0;
 		$n = 0;
 		foreach ($entries as $entry)
