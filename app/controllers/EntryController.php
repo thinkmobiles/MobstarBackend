4c23dc9a11157829b8b31f3ad59ea60a45762b8e
Matthew <matthew@goreport.net>
2014-09-28 15:59:18 +0100

    wip

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -1984,7 +1984,7 @@ class EntryController extends BaseController
 
 				$file_in = "/" . $_ENV[ 'PATH' ] . 'public/uploads/' . $file->entry_file_name . "." . $file->entry_file_type;
 
-				if(file_exists($file_in) && $file->file_type == "jpg")
+				if(file_exists($file_in) && $file->entry_file_type == "jpg")
 				{
 					$img = Image::make( $file_in );
 
@@ -2000,7 +2000,7 @@ class EntryController extends BaseController
 
 					$file->save();
 				}
-				
+
 			}
 		}
 	}
