d9fa3123eea95b17324af0be63f4170d6f4e8710
Matthew <matthew@goreport.net>
2014-10-01 17:53:34 +0100

    wip

--- app/config/mail.php
+++ app/config/mail.php
@@ -17,11 +17,6 @@ return array(
 
 	'driver' => 'mailgun',
 
-	'mailgun' => array(
-		'domain' => 'api.mobstar.com',
-		'secret' => 'key-94635a13e7b036b5a4abaa6587a1a197',
-	),
-
 	/*
 	|--------------------------------------------------------------------------
 	| SMTP Host Address
---
+++ app/config/services.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'mailgun' => array(
+		'domain' => 'api.mobstar.com',
+		'secret' => 'key-94635a13e7b036b5a4abaa6587a1a197',
+	),
+);
