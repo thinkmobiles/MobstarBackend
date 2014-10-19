e3eaa8df96f938705505b7a832f594099dbe94e8
Matt <matt@goreport.net>
2014-10-19 22:14:55 +0100

    rotation wip

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -1090,9 +1090,10 @@ class EntryController extends BaseController
 
 						$thumb = $_ENV[ 'PATH' ] . 'public/uploads/' . $filename . '-thumb.jpg';
 
-						exec('/usr/bin/ffprobe 2>&1 | grep "rotate          :"' . $file_out . '', $rotation);
+						exec('/usr/bin/ffprobe 2>&1 ' . $file_out . ' | grep "rotate          :"', $rotation);
 
-						$rotation = substr($rotation[0], 17);
+						if(isset($rotation[0]))
+							$rotation = substr($rotation[0], 17);
 
 						switch($rotation)
 						{
