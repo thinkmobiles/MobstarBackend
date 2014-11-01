8b50747d1ccbe061cd621902591c7509ddeb7fe0
Matt <matt@goreport.net>
2014-11-01 21:12:13 +0000

    wip

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -14,62 +14,63 @@ class AdminController extends BaseController
 	public function index()
 	{
 		$entries = $this->entry->all();
-		$data['entries']= [];
+		$data[ 'entries' ] = [ ];
 
 		$client = getS3Client();
 
-		foreach($entries as $entry)
+		foreach( $entries as $entry )
 		{
-			if(count($entry->file) == 0)
+			if( count( $entry->file ) == 0 )
 			{
 				continue;
 			}
-			$new = [];
+			$new = [ ];
 
-			foreach($entry->file as $file)
+			foreach( $entry->file as $file )
 			{
-				if($entry->entry_type == 'video')
+				if( $entry->entry_type == 'video' )
 				{
-					if($file->entry_file_type == 'mp4')
+					if( $file->entry_file_type == 'mp4' )
+					{
+						$new[ 'entry_file' ] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
+						$new[ 'entry_image' ] = $client->getObjectUrl( 'mobstar-1', 'thumbs/' . $file->entry_file_name . '-thumb.jpg', '+60 minutes' );
+
+					}
+				}
+				elseif( $entry->entry_type == 'audio' )
+				{
+					if( $file->entry_file_type == 'mp3' )
 					{
 						$new[ 'entry_file' ] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
 					}
-					else if($file->entry_file_type == 'jpg' || $file->entry_file_type == 'png')
-					{
-						$new['entry_image'] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
-					}
-				}
-				elseif($entry->entry_type == 'audio')
-				{
-					if($file->entry_file_type == 'mp3')
-					{
-						$new['entry_file'] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
-					}
 
-					else if($file->entry_file_type == 'jpg' || $file->entry_file_type == 'png')
+					else
 					{
-						$new['entry_image'] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
+						if( $file->entry_file_type == 'jpg' || $file->entry_file_type == 'png' )
+						{
+							$new[ 'entry_image' ] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
+						}
 					}
 				}
-				elseif($entry->entry_type == 'image')
+				elseif( $entry->entry_type == 'image' )
 				{
-					if($file->entry_file_type == 'jpg' || $file->entry_file_type == 'png')
+					if( $file->entry_file_type == 'jpg' || $file->entry_file_type == 'png' )
 					{
-						$new['entry_image'] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
+						$new[ 'entry_image' ] = $client->getObjectUrl( 'mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+60 minutes' );
 					}
 				}
 			}
 
-			$new['entry_display_name'] = $entry->user->user_display_name;
-			$new['entry_name'] = $entry->entry_description;
-			$new['entry_type'] = $entry->entry_type;
-			$new['entry_id'] = $entry->entry_id;
-			$new['entry_deleted'] = $entry->entry_deleted;
+			$new[ 'entry_display_name' ] = $entry->user->user_display_name;
+			$new[ 'entry_name' ] = $entry->entry_description;
+			$new[ 'entry_type' ] = $entry->entry_type;
+			$new[ 'entry_id' ] = $entry->entry_id;
+			$new[ 'entry_deleted' ] = $entry->entry_deleted;
 
-			$data['entries'][] = $new;
+			$data[ 'entries' ][ ] = $new;
 
 		}
 
-		return View::make('admin.home', $data);
+		return View::make( 'admin.home', $data );
 	}
 }
