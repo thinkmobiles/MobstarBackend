293450bd421d98e641740b79cb17ed0fd15be7bf
Matt <matt@goreport.net>
2014-11-02 10:33:22 +0000

    wip

--- app/views/admin/login.blade.php
+++ app/views/admin/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Laravel PHP Framework</title>
+	<title>Please Logink</title>
 
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
