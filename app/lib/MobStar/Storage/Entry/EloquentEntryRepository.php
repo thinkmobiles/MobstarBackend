c90776cd347c7ff86a7ef412d11751c803c13257
Matt <matt@goreport.net>
2014-11-05 21:26:04 +0000

    wip

--- app/lib/MobStar/Storage/Entry/EloquentEntryRepository.php
+++ app/lib/MobStar/Storage/Entry/EloquentEntryRepository.php
@@ -219,9 +219,9 @@ class EloquentEntryRepository implements EntryRepository
 		)->where( 'entry_deleted', '=', 0 )->get();
 
 
-		$query = $query->orWhereHas( 'entryTag', function ( $q ) use ( $tag_id )
+		$query = $query->whereHas( 'entryTag', function ( $q ) use ( $tag_id )
 		{
-			$q->where( 'entry_tag_tag_id', '=', $tag_id );
+			$q->orWhere( 'entry_tag_tag_id', '=', $tag_id );
 		} );
 
 		return $query->get();
