ebe5d6b33faafac90ba428330a5afc1aea9bbb7d
Anil Solanki <anil@spaceotechnologies.com>
2014-12-29 16:34:42 +0530

    email draft changes

--- app/views/emails/password.blade.php
+++ app/views/emails/password.blade.php
@@ -35,7 +35,7 @@
 </head>
 <body>
 <div>
-	<p>Your password has been reset to {{ $temporarypassword }}. Once after login you can change your password.</p>
+	<h1>Your password has been reset to {{ $temporarypassword }}. Once after login you can change your password.</h1>
 </div>
 </body>
 </html>
