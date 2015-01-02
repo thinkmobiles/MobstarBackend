a1a12cfe4d34655a29f28d102934303f049415be
Anil Solanki <anil@spaceotechnologies.com>
2015-01-02 16:28:08 +0530

    oneEntryNew function

--- app/models/Entry.php
+++ app/models/Entry.php
@@ -123,5 +123,85 @@ class Entry extends \Eloquent
 		}
 
 		return $current;
+	}
+	/////
+	public function oneEntryNew( $entry, $session, $includeUser = false )
+	{
+		mail('anil@spaceotechnologies.com',time().'oneEntryNew',print_r($entry,true));
+		$client = getS3Client();
+
+		$current = array();
+
+		$up_votes = 0;
+		$down_votes = 0;
+		foreach( $entry->vote as $vote )
+		{
+			if( $vote->vote_up == 1 && $vote->vote_deleted == 0 )
+			{
+				$up_votes++;
+			}
+			elseif( $vote->vote_down == 1 && $vote->vote_deleted == 0 )
+			{
+				$down_votes++;
+			}
+
+		}
+
+		$current[ 'id' ] = $entry->entry_id;
+		$current[ 'category' ] = $entry->category->category_name;
+		$current[ 'type' ] = $entry->entry_type;
+
+		if( $includeUser )
+		{
+			$current[ 'user' ][ 'userId' ] = $entry->entry_user_id;
+			$current[ 'user' ][ 'userName' ] = $entry->User->user_name;
+			$current[ 'user' ][ 'displayName' ] = $entry->User->user_display_name;
+			$current[ 'user' ][ 'email' ] = $entry->User->user_email;
+			$current[ 'user' ][ 'profileImage' ] = ( !empty( $entry->User->user_profile_image ) )
+				? $_ENV[ 'URL' ] . "/" . $entry->User->user_profile_cover : "";
+			$current[ 'user' ][ 'profileCover' ] = ( !empty( $entry->User->user_profile_cover ) )
+				? $_ENV[ 'URL' ] . "/" . $entry->User->user_profile_cover : "";
+			$current[ 'user' ][ 'isMyStar' ] = Star::where( 'user_star_user_id', '=', $session->token_user_id )->where( 'user_star_star_id', '=', $entry->entry_user_id )->count();
+		}
+
+		$current[ 'name' ] = $entry->entry_name;
+		$current[ 'description' ] = $entry->entry_description;
+		$current[ 'created' ] = $entry->entry_created_date;
+		$current[ 'modified' ] = $entry->entry_modified_date;
+
+		$current[ 'tags' ] = array();
+		foreach( $entry->entryTag as $tag )
+		{
+			$current[ 'tags' ][ ] = Tag::find( $tag->entry_tag_tag_id )->tag_name;
+		}
+
+		//break;
+
+		$current[ 'entryFiles' ] = array();
+		foreach( $entry->file as $file )
+		{
+			$url = $client->getObjectUrl('mobstar-1', $file->entry_file_name . "." . $file->entry_file_type, '+10 minutes');
+			$current[ 'entryFiles' ][ ] = [
+				'fileType' => $file->entry_file_type,
+				'filePath' => $url ];
+		}
+
+		$current[ 'upVotes' ] = $up_votes;
+		$current[ 'downVotes' ] = $down_votes;
+		$current[ 'rank' ] = $entry->entry_rank;
+		$current[ 'language' ] = $entry->entry_language;
+		// /print_r($entry);
+
+		if( $entry->entry_deleted )
+		{
+			$current[ 'deleted' ] = true;
+		}
+		else
+		{
+			$current[ 'deleted' ] = false;
+		}
+
+		return $current;
 	}	
+	/////	
 }
