42f0831877e141e3967ba27eba90b4ae10ebff2e
Anil Solanki <anil@spaceotechnologies.com>
2015-01-02 15:47:23 +0530

    search change

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -1927,7 +1927,7 @@ class EntryController extends BaseController
 		$term = Input::get( "term" );
 
 		$results = $this->entry->search( $term );
-		mail('anil@spaceotechnologies.com',time().'mysearch',print_r($results,true));
+		mail('anil@spaceotechnologies.com',time().'search2',print_r($results,true));
 		$status_code = 200;
 
 		if( count( $results ) == 0 )
