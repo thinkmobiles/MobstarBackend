a07b63ea432f8ce124adbb6aeb637f47cd0f466a
Anil Solanki <anil@spaceotechnologies.com>
2015-01-02 16:26:30 +0530

    search changes

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -2149,12 +2149,12 @@ class EntryController extends BaseController
 		{
 			$return = [ ];
 			$return[ 'entries' ][ ][ 'entry' ] =$results;
-			mail('anil@spaceotechnologies.com',time().'mysearch',print_r($results,true));
-			mail('anil@spaceotechnologies.com',time().'mysearchone',print_r($results[0]->entry_id,true));
-			for($i=0;$i<count($results);$i++)
+			//mail('anil@spaceotechnologies.com',time().'mysearch',print_r($results,true));
+			//mail('anil@spaceotechnologies.com',time().'mysearchone',print_r($results[0]->entry_id,true));
+			/*for($i=0;$i<count($results);$i++)
 			{
 				$return[ 'entries' ][ ][ 'entry' ] = oneEntry( $results[$i], $session, true );
-			}
+			}*/
 			/*foreach( $results as $entry )
 			{
 				$return[ 'entries' ][ ][ 'entry' ] = oneEntry( $entry, $session, true );
