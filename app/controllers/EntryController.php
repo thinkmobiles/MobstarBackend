90f7c26fedea8bd8c644ee9df8b00d680aee67c1
Anil Solanki <anil@spaceotechnologies.com>
2015-01-03 10:24:37 +0530

    search changes

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -2130,7 +2130,7 @@ class EntryController extends BaseController
 		$token = Request::header( "X-API-TOKEN" );
 		$session = $this->token->get_session( $token );		
 		$term = Input::get( "term" );
-		$results = DB::table('entries')
+		/*$results = DB::table('entries')
 		->select('entries.*')
 		->join('users', 'entries.entry_user_id', '=', 'users.user_id')
 		//->where('entries.entry_deleted', '=', '0')
@@ -2139,23 +2139,21 @@ class EntryController extends BaseController
 		->orWhere('users.user_name', 'LIKE', "%$term%")
 		->orWhere('users.user_full_name', 'LIKE', "%$term%")
 		->groupBy('entries.entry_id')
-		->get();
+		->get();*/
 		/////
-		/*$results = DB::table('entries')
+		$results = DB::table('entries')
 		->select('entries.*')
 		->join('users', 'entries.entry_user_id', '=', 'users.user_id')
-		->where(function ($query) {
-			$query->where('entries.entry_deleted', '=', 0);
-			})
-		->where(function ($query) {
+		->where(function($query)
+		{
 			$query->where('entries.entry_name', 'LIKE', "%$term%")
-				 ->orWhere('entries.entry_description', 'LIKE', "%$term%")
+				->orWhere('entries.entry_description', 'LIKE', "%$term%")
 				->orWhere('users.user_name', 'LIKE', "%$term%")
 				->orWhere('users.user_full_name', 'LIKE', "%$term%");
 		})
+		->where('entries.entry_deleted', '=', '0')		
 		->groupBy('entries.entry_id')
-		->get();*/
-		
+		->get();		
 		/////
 		
 		$status_code = 200;
