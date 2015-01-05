028551623c0f498088d8b2430ab669df98b1005a
Anil Solanki <anil@spaceotechnologies.com>
2015-01-05 13:06:21 +0530

    dummy test

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -2280,7 +2280,6 @@ class EntryController extends BaseController
 	//////
 	public function dummytest()
 	{
-		die('here');
 		$client = getS3Client();
 
 		$token = Request::header( "X-API-TOKEN" );
