87798e2a870d6142299abfb3f9c4dc7786d659ce
Anil Solanki <anil@spaceotechnologies.com>
2015-01-06 19:57:03 +0530

    add user phone field in response changes-normal login

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -140,13 +140,14 @@ class LoginController extends BaseController
 				}
 
 				Token::create( $token );
-				$isVerifiedPhone = DB::table('user_phones')->where('user_phone_user_id', '=', Auth::user()->user_id)->pluck('user_phone_verified');
+				/*$isVerifiedPhone = DB::table('user_phones')->where('user_phone_user_id', '=', Auth::user()->user_id)->pluck('user_phone_verified');
 				if(empty($isVerifiedPhone))
 				{
 					$isVerifiedPhone = '0';
 				}
 				else
 					$isVerifiedPhone = $isVerifiedPhone;
+				*/
 				//Return user id and token details:
 				$return = array(
 					'token'           => $session_key,
@@ -159,7 +160,7 @@ class LoginController extends BaseController
 						? 'http://' . $_ENV[ 'URL' ] . '/' . Auth::user()->user_profile_image : '',
 					'profileCover'    => ( !empty( Auth::user()->user_cover_image ) )
 						? 'http://' . $_ENV[ 'URL' ] . '/' . Auth::user()->user_cover_image : '',
-					'userPhone'     => $isVerifiedPhone,	
+					//'userPhone'     => $isVerifiedPhone,	
 				);
 
 				$status_code = 200;
