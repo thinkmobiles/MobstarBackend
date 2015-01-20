d0e6c0f319dae0a8e49cbe0cdeeea67c53e402f0
Anil Solanki <anil@spaceotechnologies.com>
2015-01-20 17:31:59 +0530

    phone call changes

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -929,8 +929,8 @@ class LoginController extends BaseController
 	{
 		// tell the caller that they should listen to their howl
 			// and play the recording back, using the URL that Twilio posted
-			header("content-type: text/xml");
-			return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n;
+			header("content-type: text/xml");			
+			return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 			<Response>
 			<Say>Hi - This is a call from MobStar to verify your device.Your unique code is:</Say>
 			<Pause length=\"2\"/>	
