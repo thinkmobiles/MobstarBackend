705bfd92c5856e31f1fbd9c06dcf48fef484a16a
Anil Solanki <anil@spaceotechnologies.com>
2015-01-20 19:24:18 +0530

    top user changes

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1354,7 +1354,7 @@ class UserController extends BaseController
 		{
 			$exclude[ ] = $rank->entry_id;
 		}
-		$entries = Entry->all($exclude, $order, $dir, $limit);
+		$entries = Entry::all($exclude, $order, $dir, $limit);
 		print_r($entries);
 		die;
 		//Get users greater than the cursor from
