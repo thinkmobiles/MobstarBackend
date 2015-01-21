bd74ce52b75ea7331000a2fd44e3e69728ce21fc
Anil Solanki <anil@spaceotechnologies.com>
2015-01-21 12:40:11 +0530

    follow query build

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -2342,9 +2342,9 @@ class EntryController extends BaseController
 		$query = $query->where( 'entry_rank', '>', 0 );
 		$query = $query->where( 'entry_deleted', '=', 0 );
 		$query = $query->whereNotIn( 'entries.entry_id',$exclude );
-		//$entries = $query->orderBy( $order, $dir )->limit( 10 );
-		$entries = $query;
-		$combined = $team->union($entries)->orderBy( $order, $dir )->limit( 10 )->get();
+		$entries = $query->orderBy( $order, $dir )->limit( 10 );
+		//$entries = $query;
+		$combined = $team->union($entries)->get();
 		var_dump($combined);
 		//dd(DB::getQueryLog());
 	}	
