4647dc61213499cda4056f33e42c75ebaee0e4c8
Matt <matt@goreport.net>
2015-01-26 21:59:26 +0000

    wip

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1375,7 +1375,7 @@ class UserController extends BaseController
 		}
 		$newOrderBy = implode(",",$ids);
 //		$users = User::whereIn( 'user_id', $ids )->orderByRaw(DB::raw("FIELD(user_id, '544, 426, 593, 386, 489, 519, 473')"))->get();
-		$users = User::whereIn( 'user_id', $ids )->orderByRaw(DB::raw("FIELD(user_id, '473, 519, 489, 386, 593, 426, 544')"))->get();
+		$users = User::whereIn( 'user_id', $ids )->orderByRaw(DB::raw("FIELD(user_id, 473, 519, 489, 386, 593, 426, 544)"))->get();
 		//Find total number to put in header
 		//$count = User::where( 'user_user_group', 4 )->count();
 		$count = User::whereIn( 'user_id', $ids )->orderByRaw(DB::raw("FIELD(user_id, $newOrderBy)"))->count();
