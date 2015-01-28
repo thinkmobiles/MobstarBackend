072d7cf4c4fb9a2b030bd8150f680e0bec871499
Anil Solanki <anil@spaceotechnologies.com>
2015-01-28 17:56:45 +0530

    changes

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -2400,10 +2400,10 @@ class EntryController extends BaseController
 
 		$session = $this->token->get_session( $token );
 
-		//$entries = Entry::where('entry_rank', '!=', 0)->with( 'user' )->orderBy( 'entry_rank', 'asc' )->get();
+		$entries = Entry::where('entry_rank', '!=', 0)->with( 'user' )->orderBy( 'entry_rank', 'asc' )->get();
 		//$entries = DB::table('entries')whereRaw('entry_rank != 0 and entry_deleted = 0')->with( 'user' )->orderBy( 'entry_rank', 'asc' )->get();
 		
-		$entries = DB::table('entries')
+		/*$entries = DB::table('entries')
 		->select('entries.*')
 		->join('users', 'entries.entry_user_id', '=', 'users.user_id')
 		->where('entries.entry_deleted', '=', '0')
@@ -2411,7 +2411,7 @@ class EntryController extends BaseController
             {
                 $query->where('entries.entry_rank', '!=', 0);						
             })			
-       ->orderBy( 'entry_rank', 'asc' )->get();
+       ->orderBy( 'entry_rank', 'asc' )->get();*/
 		
 		
 		$users = [ ];
