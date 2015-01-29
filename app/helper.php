84bf432ee3c531360517dfdfccd4de5bfecabbe9
Anil Solanki <anil@spaceotechnologies.com>
2015-01-29 15:45:23 +0530

    changes

--- app/helper.php
+++ app/helper.php
@@ -309,7 +309,7 @@ function getSNSClient()
 function userDetails($user)
 {
 	
-	mail('anil@spaceotechnologies.com',time(),print_r(is_object($user),true));
+	mail('anil@spaceotechnologies.com',time(),print_r($user['user_email'],true));
 	$return = [];
 	if( ( $user->user_display_name == '' ) || ( is_null( $user->user_name ) ) || ( is_null( $user->user_email ) ) )
 	{
