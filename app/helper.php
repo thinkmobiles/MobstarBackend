ef41cbe599bc87a0cfd06b380ab69e3b3bb3e27a
Anil Solanki <anil@spaceotechnologies.com>
2015-01-29 15:12:56 +0530

    changes for user details

--- app/helper.php
+++ app/helper.php
@@ -131,7 +131,7 @@ function oneUser( $user, $session, $includeStars = false )
 			if( $star->user_star_deleted == 0 )
 			{
 				$starNames = [];
-				$starNames = userDetails($star->Stars);
+				$starNames = userDetails($user);
 
 				$stars[ ] = [ 'starId'       => $star->Stars->user_id,
 							  'starName'     => $starNames['displayName'],
