33eff66a7b7f5d35db4d1ca94280909a7b6036b4
Anil Solanki <anil@spaceotechnologies.com>
2015-02-21 18:14:13 +0530

    add one parameter in star array with key isMyStar

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1333,7 +1333,7 @@ class UserController extends BaseController
 		$user = User::find($session['token_user_id']);
 
 		$return['user'] = oneUser($user, $session, true);
-
+		$return[ 'user' ][ 'isMyStar' ] = Star::where( 'user_star_user_id', '=', $session->user_id )->where( 'user_star_star_id', '=', $user->user_id )->count();
 		return Response::make( $return , 200 );
 
 	}
