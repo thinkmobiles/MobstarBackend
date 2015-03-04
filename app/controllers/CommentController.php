eadf030aaa608f19fe1311e1e10858f41dd0f06e
unknown <anil@spaceotechnologies.com>
2015-03-04 19:14:57 +0530

    changes

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -282,6 +282,7 @@ class CommentController extends BaseController
 				Notification::create( [ 'notification_user_id'      => $record->entry_user_id,
 										'notification_subject_ids'  => json_encode( [ $session->token_user_id ] ),
 										'notification_details'      => 'has commented on your entry',
+										'notification_icon'			=> 'comment.png',
 										'notification_read'         => 0,
 										'notification_entry_id'     => $record->entry_id,
 										'notification_type'         => 'Entry Comment',
