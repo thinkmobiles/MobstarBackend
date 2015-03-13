0840a0e622be154001ed4931ccf54d119b252338
unknown <anil@spaceotechnologies.com>
2015-03-13 18:31:32 +0530

    make new entry by default disabled

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -1105,7 +1105,8 @@ class EntryController extends BaseController
 
 				//Get input
 				$entry_deleted = 0;
-				/* Commented for one Demo as on request 07 March 2015
+				
+				/* Commented for one Demo as on request 07 March 2015 // Removed Comment on 13 March 2015 as on request */
 				if( Input::get( 'category' ) == 7 || Input::get( 'category' ) == 8 )
 				{
 					$entry_deleted = 0;
@@ -1114,7 +1115,7 @@ class EntryController extends BaseController
 				{
 					$entry_deleted = 1;
 				}
-				*/
+				
 				$input = [
 					'entry_user_id'      => $session->token_user_id,
 					'entry_category_id'  => Input::get( 'category' ),
