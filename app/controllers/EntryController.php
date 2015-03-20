489e054223e22dcbc33cb8919adbffe98e9592d3
unknown <anil@spaceotechnologies.com>
2015-03-20 17:09:53 +0530

    change in videoupload

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -3315,7 +3315,7 @@ class EntryController extends BaseController
 			$file_out = $_ENV[ 'PATH' ] . 'public/uploads/' . $filename . '.mp4';
 
 			// Transcode Video
-			shell_exec( '/usr/bin/ffmpeg -i ' . $file_in . ' -vf scale=306:306 -strict -2 ' . $file_out . ' 2>' . $_ENV[ 'PATH' ] . 'public/uploads/' . $filename . '-log.txt' );
+			shell_exec( '/usr/bin/ffmpeg -i ' . $file_in . ' -vf scale=306:306 -strict -vsync 2 ' . $file_out . ' 2>' . $_ENV[ 'PATH' ] . 'public/uploads/' . $filename . '-log.txt' );
 			$file->move( $_ENV[ 'PATH' ] . 'public/uploads/', $filename . '-uploaded.' . $extension );
 
 			$extension = 'mp4';
