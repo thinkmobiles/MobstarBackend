643858ef2fe13031c5616f83d1ed1c834e277818
spaceoanil <anil@spaceotechnologies.com>
2015-04-08 19:49:34 +0530

    remove backslah from tag also

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -1206,7 +1206,8 @@ class EntryController extends BaseController
 
 					foreach( $tags as $tag )
 					{
-						$this->entry->addTag( trim( $tag ), $response[ 'entry_id' ], $session->token_user_id );
+						$this->entry->addTag( trim(preg_replace('/\\\\/', '', $tag)), $response[ 'entry_id' ], $session->token_user_id );
+						//$this->entry->addTag( trim( $tag ), $response[ 'entry_id' ], $session->token_user_id );
 					}
 				}
 
