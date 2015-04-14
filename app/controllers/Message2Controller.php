be209444a189af782cddec3af7ef2e800fe72dbb
spaceoanil <anil@spaceotechnologies.com>
2015-04-14 18:51:25 +0530

    change order

--- app/controllers/Message2Controller.php
+++ app/controllers/Message2Controller.php
@@ -259,6 +259,12 @@ class Message2Controller extends BaseController
 
 		$receivedMessages = [ ];
 		$tmp_check_msgId = [ ];
+		$message_created_date = array();
+		foreach ($inventory as $key => $row)
+		{
+			$message_created_date[$key] = $row['message_created_date'];
+		}
+		array_multisort($price, SORT_ASC, $thread->messageRecipients);
 		foreach( $thread->messageRecipients as $received )
 		{
 			//if( $received->join_message_recipient_user_id == $session->token_user_id )
@@ -275,7 +281,6 @@ class Message2Controller extends BaseController
 			}
 			//}
 		}
-
 		$current[ 'messages' ] = $receivedMessages;
 
 		$current[ 'participants' ] = [ ];
