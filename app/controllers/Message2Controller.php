894b79e61dff9ba55c33ded081dc4377f57a805c
spaceoanil <anil@spaceotechnologies.com>
2015-04-14 17:18:16 +0530

    change in array

--- app/controllers/Message2Controller.php
+++ app/controllers/Message2Controller.php
@@ -263,7 +263,7 @@ class Message2Controller extends BaseController
 		{
 			//if( $received->join_message_recipient_user_id == $session->token_user_id )
 			//{
-			if( !in_array($received->join_message_recipient_message_id,$receivedMessage))
+			if( !in_array($received->join_message_recipient_message_id,$receivedMessages))
 			{
 				$receivedMessages[ ] = [
 					'message_id'         => $received->message->message_id,
