1a6742d3eaf576df938a8f6a8824ee6f953bba6e
spaceoanil <anil@spaceotechnologies.com>
2015-04-17 15:06:51 +0530

    put log

--- app/lib/MobStar/Storage/Message2/EloquentMessage2Repository.php
+++ app/lib/MobStar/Storage/Message2/EloquentMessage2Repository.php
@@ -59,6 +59,7 @@ class EloquentMessage2Repository implements Message2Repository
 			return $query->find($thread);
 		}
 		else{
+			mail('anil@spaceotechnologies.com',time(), 'I am in else');
 			$query = $query->whereHas( 'messageParticipants', function ( $query ) use ( $user )
 			{
 				$query = $query->where( 'join_message_participant_user_id', '=', $user );
