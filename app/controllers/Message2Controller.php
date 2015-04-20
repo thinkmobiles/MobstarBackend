70fb22237d30f23f562311895968b9ce8e97419f
spaceoanil <anil@spaceotechnologies.com>
2015-04-20 15:58:41 +0530

    remove query from count action

--- app/controllers/Message2Controller.php
+++ app/controllers/Message2Controller.php
@@ -852,16 +852,7 @@ public function reply()
 		$token = Request::header( "X-API-TOKEN" );
 
 		$session = $this->token->get_session( $token );
-		$count = DB::table( 'messages' )
-					->select( 'messages.message_id' )
-					->leftJoin('message_threads', 'message_threads.message_thread_thread_id', '=', 'messages.message_thread_id')
-					->leftJoin( 'join_message_participants', 'join_message_participants.join_message_participant_message_thread_id', '=', 'message_threads.message_thread_thread_id' )
-					->leftJoin( 'join_message_recipients', 'join_message_recipients.join_message_recipient_thread_id', '=', 'join_message_recipients.join_message_participant_message_thread_id' )
-					->where( 'join_message_recipients.join_message_recipient_user_id', '=', $session->token_user_id )
-					->where('join_message_recipients.join_message_recipient_read', '=', 0)
-					->where( 'join_message_participants.join_message_participant_deleted_thread', '=', 0)
-					->where( 'messages.message_deleted', '=', '0' )
-					->count();	
+			
 
 		$return[ 'notifications' ]= $count;
 
