b835e31944064315467994f9ea496403385741d7
spaceoanil <anil@spaceotechnologies.com>
2015-04-23 15:20:19 +0530

    remove duplicate participants

--- app/controllers/Message2Controller.php
+++ app/controllers/Message2Controller.php
@@ -166,14 +166,17 @@ class Message2Controller extends BaseController
 			$current[ 'read' ] = $msgread;
 			
 			$current[ 'participants' ] = [ ];
-
+			$i=0;
 			foreach( $message->messageParticipants as $participant )
 			{
-				if( $participant->user->user_id == $session->token_user_id )
+				if(!in_array($participant->user->user_id,$current[ 'participants' ][$i]->id))
 				{
-					continue;
+					if( $participant->user->user_id == $session->token_user_id )
+					{
+						continue;
+					}
+					$current[ 'participants' ][ ] = oneUser( $participant->user, $session, false );
 				}
-				$current[ 'participants' ][ ] = oneUser( $participant->user, $session, false );
 			}
 
 			$return[ 'threads' ][ ][ 'thread' ] = $current;
