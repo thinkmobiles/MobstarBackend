f1b2b96771dca7e0f7bb77e65bb0524fbd13b269
spaceoanil <anil@spaceotechnologies.com>
2015-04-23 15:37:48 +0530

    changes

--- app/controllers/Message2Controller.php
+++ app/controllers/Message2Controller.php
@@ -132,6 +132,7 @@ class Message2Controller extends BaseController
 				if($received->join_message_recipient_user_id != $session->token_user_id)
 				{
 					$msgread = $received->join_message_recipient_read;
+					$newlastMessage = received;					
 				}
 				if( $received->join_message_recipient_created > $last )
 				{
@@ -152,7 +153,7 @@ class Message2Controller extends BaseController
 			}
 
 			$current[ 'lastMessage' ][ 'messageContent' ] = $lastMessage->message->message_body;
-			$current[ 'lastMessage' ][ 'messageSender' ] = oneUser( $lastMessage->message->sender, $session );
+			$current[ 'lastMessage' ][ 'messageSender' ] = oneUser( $newlastMessage->message->sender, $session );
 			$current[ 'lastMessage' ][ 'messageReceived' ] = $lastMessage->message->message_created_date;
 
 			$msgread = MessageRecipients::where('join_message_recipient_user_id','=',$session->token_user_id)
