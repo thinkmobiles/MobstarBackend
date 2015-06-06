d61084abde5b690bdf40c5642bde2bb9cf8b6be9
spaceoanil <anil@spaceotechnologies.com>
2015-06-06 19:07:04 +0530

    commint

--- app/controllers/EntryController.php
+++ app/controllers/EntryController.php
@@ -2192,11 +2192,11 @@ class EntryController extends BaseController
 
 		foreach( $entries as $entry )
 		{
-			if(count($entry->file) <= 0)
+			if(count($entry[ 'file' ]) <= 0)
 				continue;
-			if( ( count($entry->file) < 2 ) &&  $entry->entry_type === 'audio' )
+			if( ( count($entry[ 'file' ]) < 2 ) &&  $entry['entry_type'] === 'audio' )
 				continue;
-			if( ( count($entry->file)  < 1 ) &&  $entry->entry_type === 'video' )
+			if( ( count($entry[ 'file' ])  < 1 ) &&  $entry['entry_type'] === 'video' )
 				continue;
 			$up_votes = 0;
 			$down_votes = 0;
