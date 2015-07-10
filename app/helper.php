8c5d69a40d10ef8ac31aeba968a12bb4b93b1c76
spaceoanil <anil@spaceotechnologies.com>
2015-07-10 12:52:54 +0530

    add one more condition for getuserprofile

--- app/helper.php
+++ app/helper.php
@@ -64,6 +64,47 @@ function getUserProfile( $user, $session, $normal = false )
 			$return[ 'fullName' ] = $user->GoogleUser->google_user_full_name;
 		}
 	}
+	elseif( ( !empty( $user->user_display_name ) ) && $session->token_type != 'Native' )
+	{
+		if( $session->token_type == 'Twitter' )
+		{
+			if( empty( $user->user_display_name ) )
+			{
+				$return[ 'userDisplayName' ] = $user->TwitterUser->twitter_user_display_name;
+			}
+			$return[ 'userName' ] = $user->TwitterUser->twitter_user_user_name;
+			$return[ 'fullName' ] = $user->TwitterUser->twitter_user_full_name;
+
+		}
+		elseif( $session->token_type == 'Facebook' )
+		{
+			if( empty( $user->user_display_name ) )
+			{
+				$return[ 'userDisplayName' ] = $user->FacebookUser->facebook_user_display_name;
+			}
+			if( empty( $user->user_name ) )
+			{
+				$return[ 'userName' ] = $user->FacebookUser->facebook_user_user_name;
+			}
+
+			$return[ 'fullName' ] = $user->FacebookUser->facebook_user_full_name;
+
+		}
+		elseif( $session->token_type == 'Google' )
+		{
+			//var_dump($user->GoogleUser);
+			if( empty( $user->user_display_name ) )
+			{
+				$return[ 'userDisplayName' ] = $user->GoogleUser->google_user_display_name;
+			}
+			if( empty( $user->user_name ) )
+			{
+				$return[ 'userName' ] = $user->GoogleUser->google_user_user_name;
+			}
+
+			$return[ 'fullName' ] = $user->GoogleUser->google_user_full_name;
+		}
+	}
 	else
 	{
 		$return[ 'userDisplayName' ] = $user->user_display_name;
