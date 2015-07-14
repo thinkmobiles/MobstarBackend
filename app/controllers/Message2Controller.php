54474256685818afd75f78fcbc287a581e2946fc
spaceoanil <anil@spaceotechnologies.com>
2015-07-14 10:47:29 +0530

    send message push on all device

--- app/controllers/Message2Controller.php
+++ app/controllers/Message2Controller.php
@@ -593,11 +593,14 @@ public function store()
 					) t1 left join users u on t1.device_registration_user_id = u.user_id 
 					where u.user_deleted = 0 
 					AND u.user_id = $recipient
-					order by t1.device_registration_date_created desc LIMIT 1"));
+					order by t1.device_registration_date_created desc"));
 
 				if(!empty($usersDeviceData))
 				{	
-					$this->registerSNSEndpoint($usersDeviceData[0], $message, $message_group, $name, $icon, $threadid);
+					for($i=0;$i<count($usersDeviceData);$i++)
+					{
+						$this->registerSNSEndpoint($usersDeviceData[$i], $message, $message_group, $name, $icon, $threadid);
+					}
 				}
 			}
 
@@ -850,11 +853,14 @@ public function reply()
 								) t1 left join users u on t1.device_registration_user_id = u.user_id 
 								where u.user_deleted = 0 
 								AND u.user_id = $u
-								order by t1.device_registration_date_created desc LIMIT 1"));
+								order by t1.device_registration_date_created desc"));
 					
 					if(!empty($usersData))
 					{	
-							$this->registerSNSEndpoint($usersData[0], $message, $message_group, $name, $icon, $threadid);
+							for($j=0;$j<count($usersData);$j++)
+							{
+								$this->registerSNSEndpoint($usersData[$j], $message, $message_group, $name, $icon, $threadid);
+							}
 					}
 				}
 			}
