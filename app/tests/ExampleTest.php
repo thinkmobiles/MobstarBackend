31dc21ff8d8e5c1c21d7d274a79f0b4c2bc93b26
Sergey Ignatko <SIgnatko@gmail.com>
2015-08-17 15:13:51 +0300

    Added test of GET /entry route.

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -7,6 +7,28 @@ class ExampleTest extends TestCase {
     $response = $this->call( 'GET', '/debug' );
 
     $this->assertTrue( strlen( $response->getContent() ) > 250 );
-    echo $response->getContent(), "\n";
+  }
+
+
+  public function testGetEntries()
+  {
+
+    $response = $this->call( 'GET', '/entry', array(), array(), array( 'HTTP_X-API-TOKEN' => '07516258357' ) );
+
+    $this->assertEquals( 200, $response->getStatusCode() );
+
+    $contentJSON = $response->getContent();
+
+    $this->assertNotEmpty( $contentJSON );
+
+    $content = json_decode( $contentJSON );
+
+    $this->assertINstanceOf( 'stdClass', $content );
+
+    $this->assertObjectHasAttribute( 'entries', $content );
+    $this->assertObjectHasAttribute( 'next', $content );
+
+    $entries = $content->entries;
+    $this->assertTrue( count( $entries ) == 20 );
   }
 }
