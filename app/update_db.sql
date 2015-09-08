499b52cde5a1f61b6f0faa44e7bce23cc3282021
Sergey Ignatko <SIgnatko@gmail.com>
2015-09-08 11:18:36 +0300

    Updated db structure to support default continent for user and entry.

--- app/update_db.sql
+++ app/update_db.sql
@@ -25,3 +25,32 @@ alter table `notifications` modify column notification_type enum('Entry Vote','E
 
 -- add column 'splitVideoId' to entries table to hold base entry id on with that split was made
 alter table `entries` add column `entry_splitVideoId` int default null comment 'base entry used for split';
+
+
+-- create continents table
+create table `continents` (
+  `continent_id` int not null,
+  `continent_name` varchar(100) not null,
+  primary key (`continent_id`)
+) engine InnoDB;
+
+insert into `continents`( `continent_id`, `continent_name` ) values
+(0, 'World'),
+(1, 'Africa'),
+(2, 'Asia'),
+(3, 'Europe'),
+(4, 'North America'),
+(5, 'Oceania'),
+(6, 'South America')
+;
+
+
+-- add column `user_continent` to `users` to store the user's continent
+alter table `users` add column
+  `user_continent` int not null default 0
+  comment 'the continent, the user belongs to';
+
+-- add column `entry_continent` to `entries` to store the continent to which the entry belongs
+alter table `entries` add column
+  `entry_continent` int not null default 0
+  comment 'the continent, the entry belong to';
